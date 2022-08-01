2518d11012bba9bfcd5e40863f4353cd871ae31b
Himel Sahadat <109313500+himel08@users.noreply.github.com>
2022-08-02 02:44:31 +0600

    Add files via upload

---
+++ parliament.cpp
@@ -0,0 +1,48 @@
+#include<cstdio>
+#include <windows.h>
+#include<math.h>
+# define PI 3.14159265358979323846
+
+#include <GL/gl.h>
+#include <GL/glut.h>
+
+void bird()
+{
+  glBegin(GL_LINES);
+  glColor3ub(255,255,255);
+  glVertex2f(0.2f,0.8f);
+  glVertex2f(0.18f,0.77f);
+
+  glColor3ub(255,255,255);
+  glVertex2f(0.18f,0.77f);
+  glVertex2f(0.15f,0.79f);
+
+  glColor3ub(255,255,255);
+  glVertex2f(0.13f,0.8f);
+  glVertex2f(0.11f,0.77f);
+
+  glColor3ub(255,255,255);
+  glVertex2f(0.11f,0.77f);
+  glVertex2f(0.08f,0.79f);
+
+  glEnd();
+
+}
+void display()
+{
+    
+     bird();
+
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitWindowSize(840, 640);
+    glutInitWindowPosition(50, 50);
+    glutCreateWindow("parliament bhaban,");
+    glutDisplayFunc(display);
+ glutTimerFunc(10, update, 0);
+    glutMainLoop();
+    return 0;
+}
