0b17267dd59cca3eb376c269a7204484be956afe
Himel Sahadat <109313500+himel08@users.noreply.github.com>
2022-08-24 01:45:05 +0600

    Update parliament.cpp

--- parliament.cpp
+++ parliament.cpp
@@ -98,6 +98,39 @@ void stairs()
 	glEnd();
 
 }
+void flags()
+{
+    glBegin(GL_QUADS);
+	glColor3ub(255, 255, 153);
+	glVertex2f(-0.01f, -0.5f);
+	glVertex2f(0.005f, -0.5f);
+    glVertex2f(0.005f, 0.07f);
+    glVertex2f(-0.01f, 0.07f);
+    glEnd();
+
+    glBegin(GL_QUADS);
+	glColor3ub(0, 153, 51);
+	glVertex2f(0.005f, 0.07f);
+	glVertex2f(0.27f, 0.07f);
+    glVertex2f(0.27f, -0.135f);
+    glVertex2f(0.005f, -0.135f);
+    glEnd();
+    int i;
+	GLfloat x=.13f; GLfloat y=-0.038f; GLfloat radius =.053f;
+	int triangleAmount = 20;
+	GLfloat twicePi = 2.0f * PI;
+	glBegin(GL_TRIANGLE_FAN);
+	    glColor3ub(255, 0, 0);
+		glVertex2f(x, y); // center of circle
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            x + (radius * cos(i *  twicePi / triangleAmount)),
+			    y + (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+}
+
 
 
 void display()
