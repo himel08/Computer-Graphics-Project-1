7d38c8e235813bdfba70d7985e0cd3d3c027e357
Iftekhar Ahmed <110146874+Iftekharahmed85@users.noreply.github.com>
2022-08-24 02:03:20 +0600

    Update parliament.cpp

--- parliament.cpp
+++ parliament.cpp
@@ -137,7 +137,167 @@ void display()
 {
     
      bird();
-     sky1();
+     void sky1()
+{
+    // glLoadIdentity();
+    int i;
+
+	GLfloat x=.5f; GLfloat y=.8f; GLfloat radius =.05f;
+	int triangleAmount = 20;
+	GLfloat twicePi = 2.0f * PI;
+
+	glBegin(GL_TRIANGLE_FAN);
+	    glColor3ub(30, 217, 255);
+		glVertex2f(x, y); // center of circle
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            x + (radius * cos(i *  twicePi / triangleAmount)),
+			    y + (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+GLfloat a=.55f; GLfloat b=.78f;
+
+	glBegin(GL_TRIANGLE_FAN);
+	    glColor3ub(30, 217, 255);
+		glVertex2f(a, b); // center of circle
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            a + (radius * cos(i *  twicePi / triangleAmount)),
+			    b + (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+	GLfloat c=.45f; GLfloat d=.78f;
+
+	glBegin(GL_TRIANGLE_FAN);
+	    glColor3ub(30, 217, 255);
+		glVertex2f(c, d); // center of circle
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            c + (radius * cos(i *  twicePi / triangleAmount)),
+			    d + (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+	GLfloat e=.52f; GLfloat f=.75f;
+
+	glBegin(GL_TRIANGLE_FAN);
+	    glColor3ub(30, 217, 255);
+		glVertex2f(e, f); // center of circle
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            e + (radius * cos(i *  twicePi / triangleAmount)),
+			    f+ (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+	GLfloat g=.6f; GLfloat h=.77f;
+
+	glBegin(GL_TRIANGLE_FAN);
+	    glColor3ub(30, 217, 255);
+		glVertex2f(g, h); // center of circle
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            g + (radius * cos(i *  twicePi / triangleAmount)),
+			    h+ (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+
+}
+
+void sky2()
+{
+    // glLoadIdentity();
+    int i;
+
+	GLfloat x=-.5f; GLfloat y=.8f; GLfloat radius =.05f;
+	int triangleAmount = 20;
+	GLfloat twicePi = 2.0f * PI;
+
+	glBegin(GL_TRIANGLE_FAN);
+	    glColor3ub(30, 217, 255);
+		glVertex2f(x, y); // center of circle
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            x + (radius * cos(i *  twicePi / triangleAmount)),
+			    y + (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+GLfloat a=-.55f; GLfloat b=.78f;
+
+	glBegin(GL_TRIANGLE_FAN);
+	    glColor3ub(30, 217, 255);
+		glVertex2f(a, b); // center of circle
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            a + (radius * cos(i *  twicePi / triangleAmount)),
+			    b + (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+	GLfloat c=-.45f; GLfloat d=.78f;
+
+	glBegin(GL_TRIANGLE_FAN);
+	    glColor3ub(30, 217, 255);
+		glVertex2f(c, d); // center of circle
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            c + (radius * cos(i *  twicePi / triangleAmount)),
+			    d + (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+	GLfloat e=-.52f; GLfloat f=.75f;
+
+	glBegin(GL_TRIANGLE_FAN);
+	    glColor3ub(30, 217, 255);
+		glVertex2f(e, f); // center of circle
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            e + (radius * cos(i *  twicePi / triangleAmount)),
+			    f+ (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+	GLfloat g=-.6f; GLfloat h=.77f;
+
+	glBegin(GL_TRIANGLE_FAN);
+	    glColor3ub(30, 217, 255);
+		glVertex2f(g, h); // center of circle
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            g + (radius * cos(i *  twicePi / triangleAmount)),
+			    h+ (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+
+}
+void fullsky()
+{
+     glLoadIdentity();
+    glBegin(GL_POLYGON);
+	glColor3ub(38, 154, 214);
+	glVertex2f(-1.0f, 1.0f);
+	glVertex2f(1.0f, 1.0f);
+	glVertex2f(1.0f, -0.5f);
+	glVertex2f(-1.0f, -0.5f);
+
+	glEnd();
+}
      stairs();
      flags();
 
